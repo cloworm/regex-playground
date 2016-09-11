256aacc00f5bf446460729177f4a0bb9277eaeec
cloworm <chloe.echikson@gmail.com>
2016-09-11 10:01:32 -0400

    snackbar lets user know that the permalink has been copied

--- client/Share.jsx
+++ client/Share.jsx
@@ -4,6 +4,7 @@ import ShareLink from './ShareLink.js';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import IconButton from 'material-ui/IconButton';
 import Copy from 'material-ui/svg-icons/content/content-copy';
+import Snackbar from 'material-ui/Snackbar';
 
 const styles = {
   paper: {
@@ -21,6 +22,24 @@ const styles = {
 };
 
 var Share = React.createClass({
+  getInitialState: function() {
+    return {
+      open: false
+    }
+  },
+
+  handleTouchTap: function() {
+    this.setState({
+      open: true
+    });
+  },
+
+  handleRequestClose: function() {
+    this.setState({
+      open: false
+    });
+  },
+
   render: function() {
     var shareLink = ShareLink(this.props.pattern, this.props.flags, this.props.matches);
     return (
@@ -34,10 +53,19 @@ var Share = React.createClass({
           underlineFocusStyle={styles.underlineFocus}
         />
         <CopyToClipboard text={shareLink}>
-          <IconButton tooltip="Copy to Clipboard">
+          <IconButton
+            tooltip="Copy to Clipboard"
+            onTouchTap={this.handleTouchTap}
+          >
             <Copy />
           </IconButton>
         </CopyToClipboard>
+        <Snackbar
+          open={this.state.open}
+          message='Copied to clipboard'
+          autoHideDuration={2000}
+          onRequestClose={this.handleRequestClose}
+        />
       </div>
     );
   }

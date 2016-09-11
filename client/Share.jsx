211d6a35555a94700d45dff2f8c915f934122f6d
cloworm <chloe.echikson@gmail.com>
2016-09-11 09:54:21 -0400

    added floating label and moved copy icon to the right of textbox

--- client/Share.jsx
+++ client/Share.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import ShareLink from './ShareLink.js';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import IconButton from 'material-ui/IconButton';
 import Copy from 'material-ui/svg-icons/content/content-copy';
-import ShareLink from './ShareLink.js';
 
 const styles = {
-  button: {
-    display: 'inline-block'
-  },
   paper: {
     display: 'inline-block',
     marginLeft: '0',
@@ -27,27 +24,20 @@ var Share = React.createClass({
   render: function() {
     var shareLink = ShareLink(this.props.pattern, this.props.flags, this.props.matches);
     return (
-      <div className='container-fluid'>
-        <div className='row'>
-          <div className='col-xs-12 col-sm-5' style={styles.permalink}>
-            Permalink:
-            <CopyToClipboard text={shareLink}>
-              <IconButton style={styles.button} tooltip="Copy to Clipboard">
-                <Copy />
-              </IconButton>
-            </CopyToClipboard>
-          </div>
-          <div className='col-xs-12 col-sm-7'>
-            <TextField
-              name='shareLink'
-              readOnly='readOnly'
-              fullWidth={true}
-              value={shareLink}
-              ref='shareLink'
-              underlineFocusStyle={styles.underlineFocus}
-            />
-          </div>
-        </div>
+      <div>
+        <TextField
+          name='shareLink'
+          floatingLabelText='Permalink'
+          readOnly='readOnly'
+          value={shareLink}
+          ref='shareLink'
+          underlineFocusStyle={styles.underlineFocus}
+        />
+        <CopyToClipboard text={shareLink}>
+          <IconButton tooltip="Copy to Clipboard">
+            <Copy />
+          </IconButton>
+        </CopyToClipboard>
       </div>
     );
   }
